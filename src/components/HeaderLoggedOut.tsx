b58637cdd5fb4d0e572145cec5ae98b6bb9c8c7d
KeisukeShimokawa <33191285+KeisukeShimokawa@users.noreply.github.com>
2021-10-10 03:02:18 +0900

    feat: ログイン機能を追加 /login のエンドポイントへのアクセスを追加 送信値が正しくない場合は、ログに出力する機能も追加

---
+++ src/components/HeaderLoggedOut.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import Axios from "axios";
+
+const HeaderLoggedOut = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await Axios.post("https://localhost:8080/login", {
+        username,
+        password
+      });
+      if (response.data) {
+        console.log(response.data);
+      } else {
+        console.log("incorrect username / password");
+      }
+    } catch (e) {
+      console.log("there was an error.");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="mb-0 pt-2 pt-md-0">
+      <div className="row align-items-center">
+        <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
+          <input
+            name="username"
+            className="form-control form-control-sm input-dark"
+            type="text"
+            placeholder="Username"
+            autoComplete="off"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
+          <input
+            name="password"
+            className="form-control form-control-sm input-dark"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="col-md-auto">
+          <button className="btn btn-success btn-sm">Sign In</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export { HeaderLoggedOut };
