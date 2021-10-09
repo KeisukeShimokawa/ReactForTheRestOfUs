8d18782e87f5538ff0fa07ab59fd0db5b2ae693e
KeisukeShimokawa <33191285+KeisukeShimokawa@users.noreply.github.com>
2021-10-10 01:12:54 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { HomeGuest } from "./components/HomeGuest";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <HomeGuest />
+      <Footer />
+    </>
+  );
+}
