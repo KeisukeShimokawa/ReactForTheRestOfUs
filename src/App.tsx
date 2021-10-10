8db3547e4f958526be8d1dc62830b5f12f7ea957
KeisukeShimokawa <33191285+KeisukeShimokawa@users.noreply.github.com>
2021-10-10 16:08:00 +0900

    feat: Profileコンポーネントを追加 Profileコンポーネントを追加して、 各種ユーザー情報はアプリが管理している 状態から参照する形式に変更

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { ViewSinglePost } from "./components/ViewSinglePost";
 import { FlashMessages } from "./components/FlashMessages";
 import { StateContext } from "./StateContext";
 import { DispatchContext } from "./DispatchContext";
+import { Profile } from "./components/Profile";
 
 type User = {
   token: string;
@@ -79,6 +80,9 @@ export default function App() {
           <FlashMessages />
           <Header />
           <Switch>
+            <Route path="/profile/:username">
+              <Profile />
+            </Route>
             <Route path="/" exact>
               {state.loggedIn ? <Home /> : <HomeGuest />}
             </Route>
